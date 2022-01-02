87bf1682a5b397ce2b3f48fa0f34b3640327f6a1
Uttam Kumar <kruttam76@gmail.com>
2022-01-02 14:21:32 -0500

    Deliverable 2: Create a Bubble Chart

--- charts.js
+++ charts.js
@@ -105,5 +105,26 @@ function buildCharts(sample) {
     };
     // 10. Use Plotly to plot the data with the layout. 
     Plotly.newPlot("bar", barData, barLayout)
+
+    // 1. Create the trace for the bubble chart.
+    var bubbleData = [
+       { x: otuidArry,
+         y: samleValuesArray,
+         text: otuLabelsArray,
+         mode: 'markers',
+         marker: {color: otuidArry,
+                  size: samleValuesArray  ,
+                  colorscale: "Earth"
+                },
+       }
+    ];
+    // 2. Create the layout for the bubble chart.
+    var bubbleLayout = {
+      title: "Bacteria Cultures Per Sample",
+      xaxis: {title: "OTU ID", automargin: true},
+      yaxis: {automargin: true}
+    };
+    // 3. Use Plotly to plot the data with the layout.
+    Plotly.newPlot("bubble", bubbleData, bubbleLayout); 
   });
 }
